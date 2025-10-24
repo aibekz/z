5798bf545c360a75d0e6cc42c35d50225a6789b0
aibekzhumabekov <aibek.zhumabekov@proton.me>
2025-10-24 16:15:14 -0500

    Update social media links in page.tsx to include YouTube and GitHub, replacing Instagram with Bluesky.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { FaXTwitter, FaInstagram, FaThreads, FaBluesky } from "react-icons/fa6";
+import { FaXTwitter, FaThreads, FaBluesky, FaYoutube, FaGithub } from "react-icons/fa6";
 
 export default function Home() {
   return (
@@ -36,20 +36,28 @@ export default function Home() {
             <FaThreads size={32} />
           </a>
           <a 
-            href="https://instagram.com/devzcodes" 
-            target="_blank" 
-            rel="noopener noreferrer"
-            className="text-white hover:text-black dark:text-white dark:hover:text-gray-400 transition-colors"
-          >
-            <FaInstagram size={32} />
-          </a>
-          <a 
             href="https://bsky.app/profile/devzcodes.bsky.social" 
             target="_blank" 
             rel="noopener noreferrer"
             className="text-white hover:text-black dark:text-white dark:hover:text-gray-400 transition-colors"
           >
             <FaBluesky size={32} />
+          </a>
+          <a 
+            href="https://youtube.com/@devzcodes" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="text-white hover:text-black dark:text-white dark:hover:text-gray-400 transition-colors"
+          >
+            <FaYoutube size={32} />
+          </a>
+          <a 
+            href="https://github.com/devzcodes" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="text-white hover:text-black dark:text-white dark:hover:text-gray-400 transition-colors"
+          >
+            <FaGithub size={32} />
           </a>
         </div>
       </main>
